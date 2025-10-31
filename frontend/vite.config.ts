3aa21b037c89d42a508a35512648d9bd4481cd1f
Lson Lee <lsonlee@hotmail.com>
2025-10-31 17:50:35 +0800

    feat: Allow LAN access

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -75,7 +75,7 @@ export default defineConfig(async () => ({
   server: {
     port: 1420,
     strictPort: true,
-    host: host || false,
+    host: host || "0.0.0.0", // Allow LAN access
     hmr: host
       ? {
           protocol: "ws",
