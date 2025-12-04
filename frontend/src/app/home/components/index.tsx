68cda140de00bc24d395d45b07b0948903300518
byronwang2005 <hzwx2005@qq.com>
2025-12-04 13:34:56 +0800

    clean files

--- frontend/src/app/home/components/index.tsx
+++ frontend/src/app/home/components/index.tsx
@@ -1,8 +1,5 @@
 export * from "./agent-suggestions-list";
 export { default as AgentTaskCards } from "./agent-task-cards";
-export { default as SparklineStockList } from "./sparkline-stock-list";
-export * from "./stock-card";
-export * from "./stock-chart";
 export * from "./stock-details-list";
 export { default as StockList } from "./stock-list";
 export { default as StockSearchModal } from "./stock-search-modal";
